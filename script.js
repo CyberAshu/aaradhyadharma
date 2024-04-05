c62d6048d6055c7d515ae12a2eddb2a90fe884a9
Ayush Sen <mr.ayushsen@gmail.com>
2024-04-06 03:09:27 +0530

    update remove items

--- script.js
+++ script.js
@@ -76,17 +76,3 @@ $(document).ready(function(){
 
 
 // Get in touch Form Output
-
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
